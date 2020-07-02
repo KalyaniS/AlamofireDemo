7245b338e9ce7311fa8aedef9cfb83b219f60218
KalyaniS <shinde.k.a@gmail.com>
2020-07-02 23:00:25 +0530

    Initial Commit

---
+++ AlamofireDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by kalyani on 02/07/20.
+//  Copyright © 2020 xyz. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
